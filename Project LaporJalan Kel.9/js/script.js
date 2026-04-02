952e897c73594a23a516f06a23182dbc7b23d2c3
hadityaputraemawan-eng <hadityaputraemawan@smkwikrama.sch.id>
2026-04-02 13:40:18 +0700

    uppdate1

--- Project LaporJalan Kel.9/js/script.js
+++ Project LaporJalan Kel.9/js/script.js
@@ -10,3 +10,15 @@ function changeText() {
 }
 
 changeText();
+// Dark Mode / light mode
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+  if (darkmode.classList.contains("bx-moon")) {
+    darkmode.classList.replace("bx-moon", "bx-sun");
+    document.body.classList.add("active");
+  } else {
+    darkmode.classList.replace("bx-sun", "bx-moon");
+    document.body.classList.remove("active");
+  }
+};
